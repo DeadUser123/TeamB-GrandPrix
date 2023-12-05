b297eeb4a9b7652f75e38a45bbf76a1507decd33
DeadUser123 <kevinxia2015@gmail.com>
2023-12-05 15:04:57 -0500

    Adjusted turn code
    Realized that I didn't need the delay in the turn code and it could cause some problems.

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -7,14 +7,14 @@ import frc.robot.Constants;
 public class Chassis {
     private Victor drivemotor; // the motor that powers the speed
     private Victor rotatemotor; // the motor for turning the robot
-    private Timer timer;
     // private int time = 0;
     private double current_position = 0;
-    private int direct;
 
     public Chassis() {
         drivemotor = new Victor(Constants.DRIVEMOTORPORT);
         rotatemotor = new Victor(Constants.ROTATEMOTORPORT);
+        drivemotor.set(0);
+        rotatemotor.set(0);
     }
 
     public void setspeed(double speed) {
@@ -43,15 +43,14 @@ public class Chassis {
         // rotatemotor.set(0);
         // }
         // new code: should have ramping based on
-        if (current_position != desired_position || Math.abs(current_position - desired_position) > 0.05) {
-            direct = (current_position - desired_position > 0) ? 1 : -1; // finds which direction the motor should turn
-            rotatemotor.set(0.1 * Math.abs(current_position - desired_position) * direct); // begins turning the motor
-                                                                                           // at a speed relative to
-                                                                                           // difference in current and
-                                                                                           // desired positions
-            timer.delay(0.1); // constant time delay so turning won't take too long
-            rotatemotor.set(0); // stops motor
-            current_position = desired_position; // updates current position
+        if (Math.abs(current_position - desired_position) > 0.05) {
+            rotatemotor.set(0.1 * (current_position - desired_position)); // begins turning the motor
+                                                                          // at a speed relative to
+                                                                          // difference in current and
+                                                                          // desired positions
+            current_position += 0.01 * (current_position - desired_position);
+        } else {
+            rotatemotor.set(0);
         }
     }
 }
